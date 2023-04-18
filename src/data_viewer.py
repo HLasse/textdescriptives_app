d2b99e3f23444934c4539ee358ef8f649bcde343
Ludvig <ludvig@clin.au.dk>
2023-04-18 21:36:02 +0800

    Initial application

---
+++ src/data_viewer.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+
+
+class DataViewer:
+
+    #@st.cache_data
+    def _convert_df_to_csv(self, data, **kwargs):
+        return data.to_csv(**kwargs).encode('utf-8')
+
+    def _header_and_download(self, header, data, file_name, key=None, label="Download", help="Download data"):
+        col1, col2 = st.columns([9, 2])
+        with col1:
+            st.subheader(header)
+        with col2:
+            st.write("")
+            st.download_button(
+                label=label,
+                data=self._convert_df_to_csv(data),
+                file_name=file_name,
+                key=key,
+                help=help
+            )
---
+++ src/main.py
@@ -0,0 +1,96 @@
+"""
+Dashboard for showcasing extraction of text metrics with textdescriptives.
+
+"""
+
+from io import StringIO
+import streamlit as st
+import textdescriptives as td
+import spacy
+import numpy as np
+
+from data_viewer import DataViewer
+
+text_print_lim = 500
+print_num_metrics = 5
+
+col1, col2 = st.columns([9, 2])
+with col1:
+    st.title("Extract Text Statistics")
+with col2:
+    st.image("https://github.com/HLasse/TextDescriptives/raw/main/docs/_static/icon.png")
+
+st.write("Calculate a large variety of statistics from text via the [**TextDescriptives**](https://github.com/HLasse/TextDescriptives) python package. Includes descriptive statistics and metrics related to readability and dependency distance.") 
+st.caption("Hansen, L., Olsen, L. R., & Enevoldsen, K. (2023). TextDescriptives: A Python package for calculating a large variety of statistics from text. [arXiv preprint arXiv:2301.02057](https://arxiv.org/abs/2301.02057)")
+
+input_choice = st.radio(label="Input", options=["Enter text", "Upload file"], index=0, horizontal=True, )
+split_by_line = st.checkbox(label="Split by newline", value=True)
+
+
+string_data = None
+
+
+if input_choice == "Upload file":
+
+    uploaded_file = st.file_uploader(
+        label="Choose a .txt file",
+        type=["txt"],
+        accept_multiple_files=False
+    )
+
+    if uploaded_file is not None:
+        # To convert to a string based IO:
+        string_data = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
+
+        # st.header("The uploaded text")
+        if split_by_line:
+            string_data = string_data.split("\n")
+        else:
+            string_data = [string_data]
+else:
+
+    default_text = """Little interest or pleasure in doing things?
+Feeling down, depressed, or hopeless?
+Trouble falling or staying asleep, or sleeping too much?
+Feeling tired or having little energy?
+Poor appetite or overeating?
+Feeling bad about yourself - or that you are a failure or have let yourself or your family down?"""
+
+    string_data = st.text_area(
+        label="Enter text",
+        value=default_text,
+        height=170,
+        max_chars=None
+    )
+
+    if string_data:
+        if split_by_line:
+            string_data = string_data.split("\n")
+        else:
+            string_data = [string_data]
+
+
+# Selection of model to use
+model_name = st.selectbox(label="Model", options=[
+                          "en_core_web_sm", "en_core_web_lg"], index=0)
+
+
+if string_data is not None and string_data:
+
+    # For dev purposes
+    # st.write(string_data)
+
+    # Will automatically download the relevant model (´en_core_web_lg´) and extract all metrics
+    df = td.extract_metrics(
+        text=string_data, spacy_model=model_name, metrics=None)
+
+    DataViewer()._header_and_download(
+        header="The calculated metrics",
+        data=df,
+        file_name="text_metrics.csv"
+    )
+
+    st.write("**Note**: This data frame has been transposed for readability.")
+    df = df.transpose().reset_index()
+    df.columns = ["Metric"] + [str(c) for c in list(df.columns)[1:]]
+    st.dataframe(data=df, width=None, height=None, use_container_width=True)
